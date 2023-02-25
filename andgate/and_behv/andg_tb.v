ff358a994dc98acf4e59bba3b8178c41093fa83d
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-25 10:23:43 +0530

    Create andg_tb.v

---
+++ andgate/and_behv/andg_tb.v
@@ -0,0 +1,24 @@
+module andg_tb;
+reg a;
+reg b;
+wire c;
+andg_beh uut(a,b,c);
+initial
+	begin
+		$dumpfile("andgate.vcd");
+		$dumpvars;
+	end
+initial 
+begin
+	a=0;b=0;
+	#10;
+	a=0;b=1;
+	#10
+	a=1;b=0;
+	#10;
+	a=1;b=1;
+	#10 $finish;
+end
+	
+endmodule
+
