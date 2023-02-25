ee838bddd13204a740f5ae7c7aa43d9c709d6927
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-25 10:29:29 +0530

    Create buff_tb.v

---
+++ buffer/buffer_gate/buff_tb.v
@@ -0,0 +1,26 @@
+module buffertb;
+	reg d;
+        reg en;
+	wire y;
+	buffer_g uut(d,en,y);
+	initial
+	begin
+		en = 1;
+		d = 1; # 20; 
+		if ( y == 0 ) 
+			$display ("Pass %d %d",d,y);
+		else
+			$display ("Fail %d %d",d,y);
+		en = 1;
+		d = 0; # 20; 
+		if ( y == 1 ) 
+			$display ("Pass %d %d",d,y);
+		else
+			$display ("Fail %d %d",d,y);
+	end
+  
+  initial begin 
+    $dumpfile("buffer.vcd");
+    $dumpvars;
+  end
+endmodule
