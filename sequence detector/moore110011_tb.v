27e973d91a130a8ca42668be2751ab6d64f031ac
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-26 10:15:19 +0530

    Create moore110011_tb.v

---
+++ sequence detector/moore110011_tb.v
@@ -0,0 +1,34 @@
+module testbench;
+reg x;
+reg clk;
+reg reset;
+wire z;
+seq_detector uut (
+    .x(x), 
+    .clk(clk), 
+    .reset(reset), 
+    .z(z)
+);
+
+always #5 clk = ~ clk;  
+
+initial begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1, testbench);
+ end
+
+    initial
+    begin
+    clk = 1'b0;
+    reset = 1'b1;
+    #15 reset = 1'b0;
+    begin 
+    #12 x = 0;#10 x = 0 ; #10 x = 1 ; #10 x = 1 ;
+    #12 x = 0;#10 x = 0 ; #10 x = 1 ; #10 x = 1 ;
+    #12 x = 1;#10 x = 0 ; #10 x = 0 ; #10 x = 1 ;
+    #12 x = 0;#10 x = 1 ; #10 x = 1 ; #10 x = 0 ;
+    #10 $finish;
+    end
+     
+end 
+endmodule
